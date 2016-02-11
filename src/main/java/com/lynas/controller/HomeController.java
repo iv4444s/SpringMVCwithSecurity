831827ea49f6c44a96e38dae55fbb02700018afd
Sazzad <sazzad.islam@dsinnovators.com>
2016-02-11 19:45:50 +0600

    hibernate add done

--- src/main/java/com/lynas/controller/HomeController.java
+++ src/main/java/com/lynas/controller/HomeController.java
@@ -1,5 +1,8 @@
 package com.lynas.controller;
 
+import com.lynas.model.AppUser;
+import com.lynas.service.AppUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,9 +12,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
+    @Autowired
+    AppUserService appUserService;
+
+
     @RequestMapping(value = "/")
     public String home(){
 
+        return "home";
+    }
+
+    @RequestMapping(value = "/addAppUser")
+    public String addAppUser(){
+        appUserService.insertAppUser(new AppUser(1, "user", "pass"));
         return "home";
     }
 }
